9aa85c2ba797f92c9384eea2e114cab49a8c0b83
Adam <adam.t.beatt@gmail.com>
2021-06-05 13:47:24 -0700

    Update myApp.js

--- myApp.js
+++ myApp.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const mongoose = require('mongoose').config();
-mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect(process.env.MONGO_URI);
 
 let Person;
 
